614040182ffe4d4b900350460b2f6fee762d263f
Jahnavi0506 <yattapujahnavi1801@gmail.com>
2026-04-01 13:30:08 +0530

    added analytics

--- papers/urls.py
+++ papers/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import get_papers, get_authors
+from .views import get_papers, get_authors, paper_growth, top_authors
+from .views import category_trends
 
 urlpatterns = [
     path('papers/', get_papers),
     path('authors/', get_authors),
+    path('trends/categories/', category_trends),
+    path('trends/growth/', paper_growth),
+    path('trends/authors/', top_authors),
 ]
--- papers/views.py
+++ papers/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Paper,Author,PaperAuthor
 from .serializers import PaperSerializer,AuthorSerializer,PaperAuthorSerializer
+from django.db.models import Count
 
 # Create your views here.
 @api_view(['GET'])
@@ -29,3 +30,39 @@ def get_authors(request):
     serializer=AuthorSerializer(authors,many=True)
     return Response(serializer.data)    
 
+@api_view(['GET'])
+def category_trends(request):
+    data=(
+        Paper.objects
+        .values('category')           #group by category
+        .annotate(count=Count('id')) #count papers in each category
+        .order_by('-count')          #order by count desc
+
+    )
+    return Response(data)
+
+@api_view(['GET'])
+def paper_growth(request):
+    data=(
+        Paper.objects
+        .values('published_date__year') #group by year
+        .annotate(count=Count('id'))     #count papers each year
+        .order_by('published_date__year') #order by year asc
+
+    )
+    return Response(data)
+
+from django.db.models import Count
+
+@api_view(['GET'])
+def top_authors(request):
+    data = (
+        Author.objects
+        .annotate(paper_count=Count('paperauthor'))
+        .order_by('-paper_count')
+    )
+
+    return Response([
+        {"author": a.name, "papers": a.paper_count}
+        for a in data
+    ])
